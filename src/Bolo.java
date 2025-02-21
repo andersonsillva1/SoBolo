3099a35c3013c923781d4ffd6ddf872b7b1dee67
Anderson Silva <gc910932@gmail.com>
2025-02-21 15:51:15 -0300

    Adicionei

--- src/Bolo.java
+++ src/Bolo.java
@@ -1,6 +1,55 @@
-public class Bolo {
+public abstract class Bolo implements IBolo {
     private int codigo;
     private double custo;
     private IFormato formato;
 
+    public Bolo(int codigo, double custo) {
+        setCodigo(codigo);
+        setCusto(custo);
+    }
+
+    public Bolo(int codigo, double custo, IFormato formato) {
+        this(codigo, custo);
+        setFormato(formato);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
+
+    public IFormato getFormato() {
+        return formato;
+    }
+
+    public void setFormato(IFormato formato) {
+        this.formato = formato;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Bolo bolo = (Bolo) o;
+        return codigo == bolo.codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Bolo{" +
+                "codigo=" + codigo +
+                ", custo=" + custo +
+                ", formato=" + formato +
+                '}';
+    }
 }
